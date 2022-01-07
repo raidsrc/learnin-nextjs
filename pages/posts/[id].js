9f42afdf736922a9722d3329b5c644de526794e0
Ray Louis <x1337c@gmail.com>
2022-01-06 22:34:10 -0800

    THIS SHIT'S NUTS

---
+++ pages/posts/[id].js
@@ -0,0 +1,32 @@
+import Layout from '../../components/layout'
+
+import { getAllPostIds, getPostData } from '../../lib/posts'
+
+export async function getStaticProps({ params }) {
+  const postData = getPostData(params.id)
+  return {
+    props: {
+      postData
+    }
+  }
+}
+
+export async function getStaticPaths() {
+  const paths = getAllPostIds()
+  return {
+    paths,
+    fallback: false
+  }
+}
+
+export default function Post(props) {
+  return (
+    <Layout>
+      {props.postData.title}
+      <br />
+      {props.postData.id}
+      <br />
+      {props.postData.date}
+    </Layout>
+  )
+}
